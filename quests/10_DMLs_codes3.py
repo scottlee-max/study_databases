b02b9f8645046bf27d57ca26bb5c05bdad099158
scottlee-max <windows2xp69@gmail.com>
2025-11-21 17:17:56 +0900

    10_DMLs_codes3.py

---
+++ quests/10_DMLs_codes3.py
@@ -0,0 +1,15 @@
+📌 문제 4 — UPDATE 연습
+
+✔ 요구사항
+
+id = 2 인 학생의 나이를 25로 수정하시오.
+
+UPDATE students
+SET age = 25
+WHERE name = ‘이영희’;
+
+Option : select 통한 UUID id 가져와서 삭제
+
+UPDATE students
+SET age = 25
+WHERE id = ...................^^
